9548dc502c9ff9afa24c1635d0dea200543f4983
Gabriel Messias <gamessiasjt@gmail.com>
2025-04-21 15:48:21 -0300

    feat(api) : implementando interface JpaBookRepository, que extends a interface do JpaRepository

---
+++ src/main/java/org/projetointegrador/unifio/javawithhexagonalarchitecture/adapters/outbounds/repositories/JpaBookRepository.java
@@ -0,0 +1,7 @@
+package org.projetointegrador.unifio.javawithhexagonalarchitecture.adapters.outbounds.repositories;
+
+import org.projetointegrador.unifio.javawithhexagonalarchitecture.adapters.outbounds.entities.JpaBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaBookRepository extends JpaRepository<Long, JpaBook> {
+}
