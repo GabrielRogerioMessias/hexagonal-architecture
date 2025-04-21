d638af9b38185c16adeeac0cd4203e51fa647f50
Gabriel Messias <gamessiasjt@gmail.com>
2025-04-21 15:08:39 -0300

    feat (api): first commit

---
+++ src/main/java/org/projetointegrador/unifio/javawithhexagonalarchitecture/JavaWithHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.projetointegrador.unifio.javawithhexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithHexagonalArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWithHexagonalArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/projetointegrador/unifio/javawithhexagonalarchitecture/JavaWithHexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projetointegrador.unifio.javawithhexagonalarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWithHexagonalArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
